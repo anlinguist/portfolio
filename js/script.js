d32fcadde9635fa8c3a72bca49e55d6465f46369
anlinguist <anlinguist@gmail.com>
2021-02-25 01:17:37 -0700

    make form work

--- js/script.js
+++ js/script.js
@@ -54,9 +54,9 @@ function scrollFunction() {
                     .style.textAlign = "center";
         if (sw > 500) {
             document.getElementById("site-title") 
-                        .style.fontSize = "30px";
+                        .style.fontSize = "34px";
             document.getElementById("sub-title")
-                        .style.fontSize = "22px"
+                        .style.fontSize = "26px"
         }
         else {
             document.getElementById("site-title") 
@@ -76,25 +76,60 @@ function scrollFunction() {
     } 
 }
 
+document.getElementById("formname").addEventListener('input', function() { checkvalues() });
+document.getElementById("formemail").addEventListener('input', function() { checkvalues() });
+document.getElementById("formmsg").addEventListener('input', function() { checkvalues() });
 
+function checkvalues() {
+    subname = document.getElementById("formname").textContent
+    subemail = document.getElementById("formemail").textContent
+    subdesc = document.getElementById("formmsg").textContent
+    validemail = validateEmail(subemail);
+    if (subname != "" && subemail != "" && subdesc != "" && validemail==true) {
+      document.getElementById("submit").disabled = false;
+    }
+    else {
+        document.getElementById("submit").disabled = true;
+    }
+  }
+
+function validateEmail(subemail) {
+    expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i
+    if (expression.test(subemail)) {
+        return true
+    }
+    return false
+}
     // check values during input
     // if values are good, enable the submit button
 
 document.getElementById("submit").addEventListener("click", function() {sendtheticket()}, false)
 
-function sendtheticket() {
-    // first, replace the submit button with the loader
-    // gather name
-    // gather email
-    // gather message
-    // send issue to server
-    // server will respond with status
-    // replace the loader with submit
-    // if sent, show success message
-    // if not send, show error message
-    // clear values
+async function sendtheticket() {
+    name = document.getElementById('formname').textContent
+  email = document.getElementById('formemail').textContent
+  description = document.getElementById('formmsg').textContent
+  issue = {
+    'name': name,
+    'email': email,
+    'description': description
+  }
 
-
-
-
+  let postinfo = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(issue)
+  }
+  emailendpoint = 'https://us-central1-query-readefine.cloudfunctions.net/app/sendMail'
+  let sentTicket = await fetch(emailendpoint, postinfo)
+  .then(function(response) {
+    return response.text();
+  }).then(function(data) {
+    return data
+  })
+  document.getElementById('formname').textContent = ""
+  document.getElementById('formemail').textContent = ""
+  document.getElementById('formmsg').textContent = ""
 }
